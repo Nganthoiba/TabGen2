b31071c9c31a572f716fd72bf2ecbe1587610b92
Khangenbam Nganthoiba Angom <leecba@gmail.com>
2015-12-25 12:38:29 +0530

    change

--- app/src/main/java/com/nganthoi/salai/tabgen/CreateOrg.java
+++ app/src/main/java/com/nganthoi/salai/tabgen/CreateOrg.java
@@ -20,7 +20,7 @@ import customDialogManager.CustomDialogManager;
 
 public class CreateOrg extends AppCompatActivity {
     EditText org_name,org_email,comp_name;
-    String displ_name=null,organisation_name=null,email=null,company_name=null;
+    String organisation_name=null,email=null;
     Button createOrg;
     Context _context=this;
     ConnectServer connectServer=new ConnectServer("http://188.166.210.24:8065/api/v1/organisation/create");
@@ -34,25 +34,19 @@ public class CreateOrg extends AppCompatActivity {
 
         org_name = (EditText) findViewById(R.id.org_name);
         org_email = (EditText) findViewById(R.id.org_email);
-        comp_name = (EditText) findViewById(R.id.company_name);
         createOrg = (Button) findViewById(R.id.createOrg);
 
         organisation_name = org_name.getText().toString()+"";
-        displ_name=organisation_name;
         email = org_email.getText().toString()+"";
-        company_name = comp_name.getText().toString()+"";
 
         createOrg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 organisation_name = org_name.getText().toString()+"";
-                displ_name=organisation_name;
                 email = org_email.getText().toString()+"";
-                company_name = comp_name.getText().toString()+"";
                 System.out.println("Organisation details: ");
                 System.out.println("Organisation name: "+organisation_name);
                 System.out.println("Email ID: "+email);
-                System.out.println("Company Name: "+company_name);
                 if(isValidate()){
                     JSONObject jObj = new JSONObject();
                     progDialog = new ProgressDialog(v.getContext());
@@ -60,10 +54,8 @@ public class CreateOrg extends AppCompatActivity {
                     progDialog.setIndeterminate(true);
                     progDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
                     try{
-                        jObj.put("display_name",displ_name);
                         jObj.put("name",organisation_name);
                         jObj.put("email",email);
-                        jObj.put("company_name",company_name);
                         new CreateOrganisation().execute(jObj);
                     }
                     catch(JSONException e){
@@ -85,11 +77,6 @@ public class CreateOrg extends AppCompatActivity {
         }
         else if(org_email.getText().toString().trim().length()==0){
             validationDialog = new CustomDialogManager(_context,"Empty fields","Please give an email",false);
-            validationDialog.showCustomDialog();
-            return false;
-        }
-        else if(comp_name.getText().toString().trim().length()==0){
-            validationDialog = new CustomDialogManager(_context,"Empty fields","Please enter your company name",false);
             validationDialog.showCustomDialog();
             return false;
         }
@@ -119,11 +106,10 @@ public class CreateOrg extends AppCompatActivity {
                     JSONObject jsonObject = new JSONObject(jsonString);
                     if(connectServer.responseCode==200){
                         String createdOrgDetails = "ID: "+jsonObject.getString("id")+"\n"+
-                                "Organisation Name: "+jsonObject.getString("display_name")+"\n"+
-                                "Email: "+jsonObject.getString("email")+"\n"+
-                                "Company Name: "+jsonObject.getString("company_name");
-                        CustomDialogManager cdm = new CustomDialogManager(_context,"Organisation Created","The organization "+
-                                "has been created successfully with the following details:\n"+createdOrgDetails,true);
+                                "Organisation Name: "+jsonObject.getString("name")+"\n"+
+                                "Email: "+jsonObject.getString("email");
+                        CustomDialogManager cdm = new CustomDialogManager(_context,"Organisation Created","The organization "+createdOrgDetails+
+                                " has been created successfully\n",true);
                         cdm.showCustomDialog();
                     }
                     else if(connectServer.responseCode==-1){
@@ -132,7 +118,8 @@ public class CreateOrg extends AppCompatActivity {
                         cdmError.showCustomDialog();
                     }
                     else {
-                        CustomDialogManager cdmError = new CustomDialogManager(_context,"Failed to create organisation","Some error occur.",false);
+                        CustomDialogManager cdmError = new CustomDialogManager(_context,"Failed to create organisation",
+                                jsonObject.getString("message"),false);
                         cdmError.showCustomDialog();
                     }
                 }catch(JSONException jsonExp){
